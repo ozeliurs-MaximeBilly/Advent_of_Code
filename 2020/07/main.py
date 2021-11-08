fffa09226bc3e4497f62e6e0757b6deda21d7057
Ozeliurs <ozeliurs@gmail.com>
2021-11-08 16:42:04 +0100

    debut

--- 2020/07/main.py
+++ 2020/07/main.py
@@ -1,1 +1,19 @@
 import re
+
+with open("input.txt","r",encoding="utf8") as file:
+    data = file.read()
+
+database = []
+
+for line in data.split("\n")[:-1]:
+    bag = line.split(" contain ")[0]
+    contains = [x.split(" ", 1) for x in line.split(" bags contain ")[1].strip(".").split(", ")]
+    database[bag] = contains
+
+bags = [[1, "shiny gold bags"]]
+
+while True:
+    new_bags = []
+    for bag in bags:
+        for i in range(int(bag[0])):
+            new_bags.append()
