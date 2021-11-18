a9d2cdefbd524aba74c1c05d1f2d6d96854b0833
Ozeliurs <ozeliurs@gmail.com>
2021-11-18 23:52:17 +0100

    Day 4 Part One
    FUUUUUUUCKING NEW LINE !!!!!!!!!!!!!!!!!!!!!

--- 2020/04/main.py
+++ 2020/04/main.py
@@ -1,8 +1,12 @@
+import json
+
+
 def gen_passport_dict(instring):
     output_dict = {}
     for line in instring.split("\n"):
-        for field in line.split(" "):
-            output_dict[field.split(":")[0]] = field.strip().split(":")[1]
+        if line != "":
+            for field in line.split(" "):
+                output_dict[field.split(":")[0]] = field.strip().split(":")[1]
     return output_dict
 
 
@@ -14,50 +18,13 @@ def validate(passport):
     return not any(missing)
 
 
-def valid(passport):
-    missing = []
-    looking_for = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
-    for field in looking_for:
-        missing.append(field not in passport)
-    if not any(missing):
-        try:
-            a = int(passport["byr"])
-        except ValueError:
-            return False
-
-        try:
-            a = int(passport["iyr"])
-        except ValueError:
-            return False
-
-        try:
-            a = int(passport["pid"])
-        except ValueError:
-            return False
-
-        try:
-            a = int(passport["eyr"])
-        except ValueError:
-            return False
-
-        if not (len(passport["ecl"]) == 3):
-            return False
-
-        if not ( "#" in passport["hcl"] and len(passport["hcl"]) == 7 ):
-            return False
-
-        if not ("cm" in passport["hgt"]):
-            return False
-    return True
-
-
 def main():
     with open("input.txt", "r", encoding="utf8") as infile:
         intext = infile.read()
 
     passport_dict_list = []
 
-    for passport in intext.split("\n\n")[:-1]:
+    for passport in intext.split("\n\n"):
         passport_dict_list.append(
             gen_passport_dict(passport)
         )
@@ -65,8 +32,7 @@ def main():
     valid_passport = 0
 
     for passport in passport_dict_list:
-        if validate(passport) and valid(passport):
-            print(passport["byr"], passport["iyr"], passport["eyr"], passport["hgt"], passport["hcl"], passport["ecl"], passport["pid"], passport)
+        if validate(passport):
             valid_passport += 1
 
     print(f"There are {valid_passport} valid passports.")
